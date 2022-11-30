f7ef8c1429130432eeb6173c7920d1bbd751eba3
Charles Santana <selrahsan@protonmail.com>
2022-11-30 13:52:08 -0300

    Initial Commit

---
+++ CharpEmoji.SRC/Program.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+
+Console.Clear();
+
+AnsiConsole.Status()
+    .Start("Emojis Populares", ctx => 
+    {
+        
+        AnsiConsole.MarkupLine("\nAlien :alien:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Raiva :angry_face:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Bebe :baby:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Rindo :beaming_face_with_smiling_eyes:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Cerveja :beer_mug:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Gato :cat_face:");
+        Thread.Sleep(1000);
+        
+        AnsiConsole.MarkupLine("Outros modelos de Emojis em :");
+        AnsiConsole.MarkupLine( ":backhand_index_pointing_right: https://spectreconsole.net/appendix/emojis :backhand_index_pointing_left:");
+    });
---
+++ README.md
@@ -0,0 +1,38 @@
+
+#  Emoji em C#
+![](https://img.shields.io/badge/.NET-6.0-blue) ![](https://img.shields.io/badge/NuGuet-Spectre.Console-PURPLE)
+
+Exemplo Emoji em .NET C# COnsole Aplication
+
+## Dependência:
+* SDK .NET 6.0
+
+### NuGet Utilizado
+* package Spectre.Console
+
+### Clonar respositório :
+```bash
+git clone https://github.com/selrahcsan/CharpEmojigit  
+```
+
+### Acessar Pasta modo CLI :
+```bash
+cd CharpEmoji
+cd CharpEmoji.SRC   
+```
+
+### Restaurar Projeto:
+```bash
+dotnet restore   
+```
+
+### Compilar:
+
+```bash
+dotnet build   
+```
+
+### Executar:
+```bash
+dotnet run   
+```
